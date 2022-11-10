768d07fc1fe575089506c7a05e471351c10f58b5
Manjukamatagi <manjunathka@maveric-systems.com>
2022-11-10 16:15:00 +0530

    pushed initial code to master

--- src/main/java/com/maveric/userservice/controller/userController.java
+++ src/main/java/com/maveric/userservice/controller/userController.java
@@ -1,88 +1,14 @@
 package com.maveric.userservice.controller;
 
-import com.maveric.userservice.Entity.User;
-import com.maveric.userservice.Service.UserService;
-import com.maveric.userservice.dto.UserDto;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "users")
 public class userController {
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    ModelMapper modelMapper;
-
-    /**
-    * Find by id done
-    * */
-    @GetMapping(value = "/{id}")
-    public User getUser(@PathVariable("id") Integer userId){
-
-         return userService.getUserById(userId);
-
-
-    }
-/**
- * Create user done
- */
-
-    @PostMapping
-    public String createUser(@RequestBody  User user){
-//        System.out.println(user.toString());
-//        User user1=modelMapper.map(userDto,User.class);
-
-     String msg=
-             userService.createUser(user);
-     return msg;
-
-    }
-
-
-
-
-
-    /**
-     * Update Userby ID
-     * @param user
-     * @param userId
-     * @return
-     */
-    @PutMapping("/{id}")
-    public User
-    updateUser(@RequestBody User user,
-               @PathVariable("id") int userId)
-    {
-        return userService.updateUser(userId,user);
-
-    }
-
-    /**
-     * Get ALL USERS
-     * @return
-     */
-    @GetMapping()
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
-    }
-
-    /**
-     * Delete User by ID
-     * @param userId
-     * @return
-     */
-    @DeleteMapping("/{id}")
-    public String
-    deleteUser(@PathVariable("id") int userId)
-    {
-        return userService.deleteUser(userId);
-
+    @GetMapping(value = "/hello")
+    public String SayHello(){
+        return "Hello, this is from user service";
     }
 }
