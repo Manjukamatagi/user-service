d29a99091adb094fb7a3da5abf6a6471ec018eeb
Manjukamatagi <manjunathka@maveric-systems.com>
2022-11-17 06:35:53 +0530

    code cleanup

--- src/main/java/com/maveric/userservice/exception/UserNotFoundForIdException.java
+++
@@ -1,9 +0,0 @@
-package com.maveric.userservice.exception;
-
-public class UserNotFoundForIdException extends Exception{
-    public UserNotFoundForIdException(String message){
-        super(message);
-    }
-
-
-}
