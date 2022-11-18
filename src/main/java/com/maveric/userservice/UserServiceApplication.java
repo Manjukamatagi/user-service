c2a3c4ca58154c6c6f8eea4aa74b5b9f83729bae
Manjukamatagi <manjunathka@maveric-systems.com>
2022-11-18 11:43:25 +0530

    Added SecurityAutoConfiguration.clss annotation to UserService Application

--- src/main/java/com/maveric/userservice/UserServiceApplication.java
+++ src/main/java/com/maveric/userservice/UserServiceApplication.java
@@ -3,11 +3,12 @@ package com.maveric.userservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 @EnableEurekaClient
 public class UserServiceApplication {
 
