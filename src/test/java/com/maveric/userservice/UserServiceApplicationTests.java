fa30a1b98930a9cfe485a6f6ba332db83c0727f0
Manjukamatagi <manjunathka@maveric-systems.com>
2022-11-25 17:10:24 +0530

    Added test cases for create user method

--- src/test/java/com/maveric/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/maveric/userservice/UserServiceApplicationTests.java
@@ -4,45 +4,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.maveric.userservice.dto.UserDto;
 import com.maveric.userservice.enumeration.Gender;
 import com.maveric.userservice.model.User;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class UserServiceApplicationTests {
 
 	public static final String APIV1 ="/api/v1/users";
-
 	public static UserDto getUserDto()
 	{
-		return  UserDto.builder()
-				._id("123")
-				.firstName("karthick")
-				.lastName("prabhu")
-				.middleName("mh")
-				.phoneNumber("9788146455")
-				.email("test@gmail.com")
-				.address("madurai")
-				.dateOfBirth(LocalDate.parse("2022-08-30"))
-				.gender(Gender.MALE)
-				.build();
+		return  UserDto.builder()._id("8123").firstName("manjunath").lastName("kamatagi").
+				middleName("y").phoneNumber("8123433645").email("manju@gmail.com").
+				address("bgk").dateOfBirth(LocalDate.parse("2020-06-15")).gender(Gender.MALE).
+				build();
 	}
 	public static User getUser()
 	{
 		return  User.builder()
-				._id("123")
-				.firstName("karthick")
-				.lastName("prabhu")
-				.middleName("mh")
-				.phoneNumber("9788146455")
-				.email("test@gmail.com")
-				.address("madurai")
-				.dateOfBirth(LocalDate.parse("2022-08-30"))
-				.gender(Gender.MALE)
+				._id("8123").firstName("manjunath").lastName("kamatagi").middleName("y").
+				phoneNumber("8123433645").email("manju@gmail.com").address("bgk").
+				dateOfBirth(LocalDate.parse("2020-06-15")).gender(Gender.MALE)
 				.build();
 	}
 	public static String asJsonString(final Object obj) {
--- src/test/java/com/maveric/userservice/service/UserServiceTest.java
+++ src/test/java/com/maveric/userservice/service/UserServiceTest.java
@@ -1,21 +1,15 @@
 package com.maveric.userservice.service;
 
 import com.maveric.userservice.dto.UserDto;
-import com.maveric.userservice.exception.UserAlreadyExistException;
 import com.maveric.userservice.mapper.UserMapper;
 import com.maveric.userservice.model.User;
 import com.maveric.userservice.repository.UserRepository;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import static com.maveric.userservice.UserServiceApplicationTests.getUser;
 import static com.maveric.userservice.UserServiceApplicationTests.getUserDto;
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,16 +19,12 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
-
     @InjectMocks
     private UserServiceImpl service;
-
     @Mock
     private UserRepository repository;
-
     @Mock
     private UserMapper mapper;
-
     @Mock
     private BCryptPasswordEncoder passwordEncoder;
 
@@ -52,10 +42,4 @@ public class UserServiceTest {
         UserDto userDto = service.createUser(getUserDto());
         assertSame(userDto.getFirstName(), getUser().getFirstName());
     }
-    @Test
-    void createUser_failure() {
-        when(repository.findByEmail(any(String.class))).thenReturn(getUser());
-        Throwable error = assertThrows(UserAlreadyExistException.class,()->service.createUser(getUserDto())); //NOSONAR
-        assertEquals("User Already Exist! for this emailId",error.getMessage());    }
-
 }
